b9425e5d0911e07926fc47057ef03ba4d9f9be02
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2024-06-11 11:03:31 -0400

    conversation queue and continue button

--- Assets/Scripts/ConversationController.cs
+++ Assets/Scripts/ConversationController.cs
@@ -9,9 +9,13 @@ public class ConversationController : MonoBehaviour
     public VoiceflowController voiceflowController;
 
     public TextMeshProUGUI outputTextBox;
-    public Queue<string> conversationQueue = new Queue<string>();
-    public string textPayload = "Hello";
+    public string textPayload = "";
+    public GameObject readingItems;
+    public GameObject inputItems;
 
+    private bool isWaitingForResponse = false;
+    private bool isReadingResponse = false;
+    private Queue<string> conversationQueue = new Queue<string>();
     private ResponseHandlerPackage responseHandlerPackage = new ResponseHandlerPackage();
 
     public void SendButtonClicked() {
@@ -40,19 +44,44 @@ public class ConversationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isReadingResponse) {
+            readingItems.SetActive(true);
+            inputItems.SetActive(false);
+        } else if (isWaitingForResponse) {
+            readingItems.SetActive(false);
+            inputItems.SetActive(false);
+        } else {
+            readingItems.SetActive(false);
+            inputItems.SetActive(true);
+        }
     }
 
-    
+    void SetOutputText(string newText) {
+        outputTextBox.text = newText;
+    }
 
     public void EnqueueChatMessage(string newText) {
-        outputTextBox.text += newText + "\n";
-        // conversationQueue.Enqueue(newText);
+        isWaitingForResponse = false;
+        Debug.Log("EnqueueChatMessage: " + newText);
+        Debug.Log("Conversation queue count: " + conversationQueue.Count);
+        if (isReadingResponse) {
+            conversationQueue.Enqueue(newText);
+        } else {
+            SetOutputText(newText);
+        }
+        isReadingResponse = true;
     }
 
     public void HandleCustomEvent(int value) {
+        isWaitingForResponse = false;
         Debug.Log("Custom event: " + value);
     }
 
-
+    public void ContinueButtonClicked() {
+        if (conversationQueue.Count > 0) {
+            SetOutputText(conversationQueue.Dequeue());
+        } else {
+            isReadingResponse = false;
+        }
+    }
 }

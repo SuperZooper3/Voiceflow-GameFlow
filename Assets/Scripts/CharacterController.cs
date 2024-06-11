ce8b8528785c928ec1189a673872bce9a8ddae3f
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2024-06-11 11:14:44 -0400

    add madeline face, create controller

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterController : MonoBehaviour
+{
+    public Texture2D normalFace;
+    public Texture2D sadFace;
+    public Texture2D angryFace;
+    public Texture2D distractedFace;
+    public Texture2D panicFace;
+    public Texture2D suprisedFace;
+    public Texture2D upsetFace;
+    public Texture2D phoneFace;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
